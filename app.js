53a366294d2b8f6ec541c6c64424c0d5bf49e7e1
Hari-Kiri <pratamahari89@gmail.com>
2023-07-08 06:35:01 +0000

    Set indent using tab.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express'
+
 const app = express()
 const port = 80
 
 app.get('/create', (req, res) => {
-  res.send('Create service')
+  	res.send('Create service')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  	console.log(`Example app listening on port ${port}`)
 })
